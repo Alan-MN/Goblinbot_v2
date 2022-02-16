1072270d1bb6f44976aa314467641bbcacacf49b
alan <alann678n@gmail.com>
2022-02-16 13:53:04 -0300

    implementação do sistema de queue//problema mensagens

--- Goblinbot_v3.py
+++ Goblinbot_v3.py
@@ -1,26 +1,38 @@
 import discord
 from discord.ext import commands
 from musica import Musica
+
+
 client = commands.Bot(command_prefix="?")
 token = open('token.txt','r')
+QUEUE_MUSICA = []
 
 
 @client.command(aliases=['p'])
 async def play(ctx, url: str):
     botinvc = ctx.guild.me.voice
-    if ctx.author.voice and ctx.author.voice.channel:
-        canal_voz = ctx.author.voice.channel
-    else:
-        await ctx.send('você precisa estar em um canal de voz para tocar música')
-        return
     if not botinvc:
-        await canal_voz.connect()
+        if ctx.author.voice and ctx.author.voice.channel:
+            canal_voz = ctx.author.voice.channel
+            await canal_voz.connect()
+        else:
+            await ctx.send('você precisa estar em um canal de voz para tocar música')
+            return
     musica = Musica(url)
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.play(discord.FFmpegPCMAudio(musica.True_url))
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 1
-    await ctx.send(f'Tocando agora: {musica.nome}')
+    if voice.is_playing():  
+        QUEUE_MUSICA.append(musica)
+        await ctx.send(f'{musica.nome} foi adicionado a fila')
+    else:
+        voice.play(discord.FFmpegPCMAudio(musica.True_url))
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 1
+        await ctx.send(f'Tocando agora: {musica.nome}')
+
+    if len(QUEUE_MUSICA):
+        prox_musica = QUEUE_MUSICA.pop(0)
+        await play(ctx,prox_musica.link)
+
 
 
 @client.command(aliases=['l'])
